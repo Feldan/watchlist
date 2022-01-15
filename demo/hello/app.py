660b22e81f07982b61afdd7b1e1995bda27c3184
feldan <zhxllongge@outlook.com>
2022-01-15 16:33:11 +0800

    I'm ready!

---
+++ demo/hello/app.py
@@ -0,0 +1,57 @@
+# -*- encoding: utf-8 -*-
+'''
+@file        :app.py
+@Author      :feldan
+@url https://blog.feldan.top
+@Date        :2022/01/15 15:53:42
+'''
+from apiflask import APIFlask, Schema, input, output, abort
+from apiflask.fields import Integer, String
+from apiflask.validators import Length, OneOf
+
+app = APIFlask(__name__)
+
+pets = [
+    {'id': 0, 'name': 'Kitty', 'category': 'cat'},
+    {'id': 1, 'name': 'Coco', 'category': 'dog'}
+]
+
+
+class PetInSchema(Schema):
+    name = String(required=True, validate=Length(0, 10))
+    category = String(required=True, validate=OneOf(['dog', 'cat']))
+
+
+class PetOutSchema(Schema):
+    id = Integer()
+    name = String()
+    category = String()
+
+
+@app.get('/')
+def say_hello():
+    # returning a dict equals to use jsonify()
+    return {'message': 'Hello!'}
+
+
+@app.get('/pets/<int:pet_id>')
+@output(PetOutSchema)
+def get_pet(pet_id):
+    if pet_id > len(pets) - 1:
+        abort(404)
+    # you can also return an ORM/ODM model class instance directly
+    # APIFlask will serialize the object into JSON format
+    return pets[pet_id]
+
+
+@app.patch('/pets/<int:pet_id>')
+@input(PetInSchema(partial=True))
+@output(PetOutSchema)
+def update_pet(pet_id, data):
+    # the validated and parsed input data will
+    # be injected into the view function as a dict
+    if pet_id > len(pets) - 1:
+        abort(404)
+    for attr, value in data.items():
+        pets[pet_id][attr] = value
+    return pets[pet_id]
